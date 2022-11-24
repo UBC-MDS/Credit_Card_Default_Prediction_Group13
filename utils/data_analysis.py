1f04755fc4c8c4e8661d2d8fac655eee41cfcd0d
hcwang24 <hcwang24@student.ubc.ca>
2022-11-23 22:07:30 -0800

    Fixing output issue with data_analysis.py

--- utils/data_analysis.py
+++ utils/data_analysis.py
@@ -7,18 +7,38 @@ Arguments:
 
 
 """
-from docopt import docopt
-import numpy as np
+
+# Imports
+import os
+import sys
 import pandas as pd
-from sklearn.compose import ColumnTransformer, make_column_transformer
+import numpy as np
+import matplotlib.pyplot as plt
+from hashlib import sha1
+from docopt import docopt
 from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.compose import ColumnTransformer, make_column_transformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler, OrdinalEncoder
+from sklearn.dummy import DummyClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import RidgeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import RandomizedSearchCV, cross_validate, train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_curve
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import classification_report
+from scipy.stats import randint
 
 
-def perform_ml_analysis(X_train_enc, X_test_enc, Y_train_data_path, Y_test_data_path):
-    if(not os.path.exists(out_folder)):
-        os.mkdir(out_folder)
-        
+def perform_ml_analysis(train_data, test_data, out_path):
+    if not os.path.exists(out_path):
+        os.mkdir(out_path)
+
     headernames = [
         "ID",
         "LIMIT_BAL",
@@ -46,17 +66,20 @@ def perform_ml_analysis(X_train_enc, X_test_enc, Y_train_data_path, Y_test_data_
         "PAY_AMT6",
         "default payment next month",
     ]
-        
-    train_df = pd.read_csv(train_data_path, index_col=0, names=headernames, skiprows=1, encoding="utf-8")
-    test_df = pd.read_csv(test_data_path, index_col=0, names=headernames, skiprows=1, encoding="utf-8")
-        
-    
+
+    train_df = pd.read_csv(
+        train_data, index_col=0, names=headernames, skiprows=1, encoding="utf-8"
+    )
+    test_df = pd.read_csv(
+        test_data, index_col=0, names=headernames, skiprows=1, encoding="utf-8"
+    )
+
     # 1. Create the column transformer / preprocessor
-    # For the numeric features, we will Standardize the numbers. 
+    # For the numeric features, we will Standardize the numbers.
     # For the two categorical features, we will apply OneHotEncoder on them to convert
-    # the column into multiple binary features. 
+    # the column into multiple binary features.
     # Many other features already have good encoding, so we don't
-    # have to modify them (passthrough). 
+    # have to modify them (passthrough).
 
     numeric_features = [
         "LIMIT_BAL",
@@ -104,8 +127,15 @@ def perform_ml_analysis(X_train_enc, X_test_enc, Y_train_data_path, Y_test_data_
     )
 
     # Show the preprocessor
-    preprocessor
-    
+    print(
+        """
+    ################################################
+    #        Column_transformer                    #
+    ################################################
+    """
+    )
+    print(preprocessor)
+
     # 2. Fit and transform on the training data
     X_train = train_df.drop(columns=["default payment next month"])
     X_test = test_df.drop(columns=["default payment next month"])
@@ -121,7 +151,9 @@ def perform_ml_analysis(X_train_enc, X_test_enc, Y_train_data_path, Y_test_data_
     # transformed data
     X_train_transformed = preprocessor.transform(X_train)
     ohe_features = (
-        preprocessor.named_transformers_["onehotencoder"].get_feature_names_out().tolist()
+        preprocessor.named_transformers_["onehotencoder"]
+        .get_feature_names_out()
+        .tolist()
     )
 
     # Code to get all the feature names
@@ -130,31 +162,15 @@ def perform_ml_analysis(X_train_enc, X_test_enc, Y_train_data_path, Y_test_data_
     X_train_enc = pd.DataFrame(X_train_transformed, columns=feature_names)
 
     # Show the transformed data
-    X_train_enc
-    
-    import sys
-    from hashlib import sha1
-
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import pandas as pd
-    from sklearn.dummy import DummyClassifier
-    from sklearn.feature_extraction.text import CountVectorizer
-    from sklearn.linear_model import LogisticRegression
-    from sklearn.model_selection import (
-        GridSearchCV,
-        RandomizedSearchCV,
-        cross_validate,
-        train_test_split,
+    print(
+        """
+    ################################################
+    #        X_train_transformed                   #
+    ################################################
+    """
     )
-    from sklearn.neighbors import KNeighborsClassifier
-    from sklearn.naive_bayes import BernoulliNB, MultinomialNB
-    from sklearn.pipeline import Pipeline, make_pipeline
-    from sklearn.svm import SVC
-    from sklearn.tree import DecisionTreeClassifier
-    from sklearn.linear_model import RidgeClassifier
-    from sklearn.ensemble import RandomForestClassifier
-    
+    print(X_train_enc)
+
     models = {
         # "dummy": DummyClassifier(random_state=123),
         "Decision Tree": DecisionTreeClassifier(random_state=123),
@@ -183,7 +199,7 @@ def perform_ml_analysis(X_train_enc, X_test_enc, Y_train_data_path, Y_test_data_
                 scoring=classification_metrics,
             )
         )
-        
+
     # code below modified from :https://stackoverflow.com/questions/13575090/
     #              construct-pandas-dataframe-from-items-in-nested-dictionary
     cross_val_results_df = (
@@ -194,27 +210,36 @@ def perform_ml_analysis(X_train_enc, X_test_enc, Y_train_data_path, Y_test_data_
             },
             axis=0,
         )
-        .T.style.format(
-            precision=2  # Pandas `.style` does not honor previous rounding via `.round()`
-        )
-        .background_gradient(
-            axis=None,
-            vmax=1,
-            vmin=0,  # Color cells based on the entire matrix rather than row/column-wise
-        )
+        .T
+        # .style.format(
+        #     precision=2  # Pandas `.style` does not honor previous rounding via `.round()`
+        # )
+        # .background_gradient(
+        #     axis=None,
+        #     vmax=1,
+        #     vmin=0,  # Color cells based on the entire matrix rather than row/column-wise
+        # )
     )
 
-    cross_val_results_df
-    
-    # Selecting RandomForestClassifier for model hyperparameter optimization. 
-    from scipy.stats import expon, lognorm, loguniform, randint, uniform, norm
-    from sklearn.model_selection import RandomizedSearchCV
+    # Show cross validation results for different modesl
+    print(
+        """
+    ################################################
+    #      CV Results from different models        #
+    ################################################
+    """
+    )
+    print(cross_val_results_df)
+
+    # We select RandomForestClassifier for model hyperparameter optimization.
 
     rf_cla = make_pipeline(preprocessor, RandomForestClassifier())
     # rf_cla.fit(X_train, y_train)
 
-    param_dist = {"randomforestclassifier__max_depth": randint(1, 500),
-                 "randomforestclassifier__class_weight": ["balanced", None]}
+    param_dist = {
+        "randomforestclassifier__max_depth": randint(1, 500),
+        "randomforestclassifier__class_weight": ["balanced", None],
+    }
 
     random_search = RandomizedSearchCV(
         rf_cla,
@@ -228,14 +253,36 @@ def perform_ml_analysis(X_train_enc, X_test_enc, Y_train_data_path, Y_test_data_
 
     random_search.fit(X_train, y_train)
 
-    random_search.best_estimator_
-    
+    # Best estimator from random search
+    print(
+        """
+    ################################################
+    #            Best estimator                    #
+    ################################################
+    """
+    )
+    print(random_search.best_estimator_)
+
     # This is an improvement compared to the mean validation f1 score using default parameters.
-    random_search.best_score_
-    
+    print(
+        """
+    ################################################
+    #      f1 score of the best estimator          #
+    ################################################
+    """
+    )
+    print(random_search.best_score_)
+
     # This is the optimized hyperparameter.
-    random_search.best_params_
-    
+    print(
+        """
+    ################################################
+    #      parameters of the best estimator        #
+    ################################################
+    """
+    )
+    print(random_search.best_params_)
+
     # Generate a table showing feature importances.
     feat_importance = random_search.best_estimator_.named_steps[
         "randomforestclassifier"
@@ -249,27 +296,56 @@ def perform_ml_analysis(X_train_enc, X_test_enc, Y_train_data_path, Y_test_data_
     )
 
     feat_names = numeric_features + ohe_names + passthrough_features
-
-    pd.DataFrame({"Feature": feat_names, "Importance": feat_importance}).sort_values(
-        by="Importance", ascending=False
+    
+    # This is the feature importances of the optimized model.
+    print(
+        """
+    ################################################
+    #   Feature importances of the best estimator  #
+    ################################################
+    """
     )
-    
-    
+    feature_table = pd.DataFrame(
+        {"Feature": feat_names, "Importance": feat_importance}
+    ).sort_values(by="Importance", ascending=False)
+    print(feature_table)
+
     # Add confusion matrix, predict score on test data.
-    from sklearn.metrics import ConfusionMatrixDisplay
-
-    cm = ConfusionMatrixDisplay.from_estimator(
-        random_search.best_estimator_, X_test, y_test, values_format="d", display_labels=["Non default", "default"]
+    print(
+        """
+    ################################################
+    #   Confusion matrix of the best estimator     #
+    ################################################
+    """
     )
-    
-    # Generate the classification report
-    from sklearn.metrics import classification_report
-    y_pred = random_search.best_estimator_.predict(X_test)
-    print(classification_report(y_test, y_pred, target_names=["Non default", "default"]))
-    
-    # Evaluate the Precision-Recall curve of the optimized model. 
+    cm = ConfusionMatrixDisplay.from_estimator(
+        random_search.best_estimator_,
+        X_test,
+        y_test,
+        values_format="d",
+        display_labels=["Non default", "default"],
+    )
+    predictions = random_search.best_estimator_.predict(X_test)
+    TN, FP, FN, TP = confusion_matrix(y_test, predictions).ravel()
+    print("Confusion matrix for default payment data set")
+    print(cm.confusion_matrix)
 
-    from sklearn.metrics import precision_recall_curve
+    # Generate the classification report
+
+    y_pred = random_search.best_estimator_.predict(X_test)
+
+    print(
+        """
+    ################################################
+    #             Classification report            #
+    ################################################
+    """
+    )
+    print(
+        classification_report(y_test, y_pred, target_names=["Non default", "default"])
+    )
+
+    # Evaluate the Precision-Recall curve of the optimized model.
 
     precision, recall, thresholds = precision_recall_curve(
         y_test, random_search.best_estimator_.predict_proba(X_test)[:, 1]
@@ -284,13 +360,15 @@ def perform_ml_analysis(X_train_enc, X_test_enc, Y_train_data_path, Y_test_data_
         markersize=10,
         label="threshold 0.5",
     )
-    plt.legend(loc="best");
-    plt.savefig('../results/precision_recall.png')
-    
-    # Evaluate the Receiver Operating Characteristic (ROC) curve of the optimized model. 
-    from sklearn.metrics import roc_curve
+    plt.legend(loc="best")        
+    plt.savefig(out_path + "/precision_recall.png")
+    print("Precision_recall curve plot saved as "+out_path+"/precision_recall.png")
 
-    fpr, tpr, thresholds = roc_curve(y_test, random_search.best_estimator_.predict_proba(X_test)[:, 1])
+    # Evaluate the Receiver Operating Characteristic (ROC) curve of the optimized model.
+
+    fpr, tpr, thresholds = roc_curve(
+        y_test, random_search.best_estimator_.predict_proba(X_test)[:, 1]
+    )
     plt.plot(fpr, tpr, label="ROC Curve")
     plt.xlabel("FPR")
     plt.ylabel("TPR (recall)")
@@ -304,30 +382,22 @@ def perform_ml_analysis(X_train_enc, X_test_enc, Y_train_data_path, Y_test_data_
         markersize=10,
         label="threshold 0.5",
     )
-    plt.legend(loc="best");
-    plt.savefig('../results/roc.png')
-    
-    # Finally, check the f1_score of the test data with our optimized model. 
+    plt.legend(loc="best")
+    plt.savefig(out_path + "/roc.png")
+    print("Receiver Operating Characteristic (ROC) curve plot saved as "+ out_path + "/roc.png")
+
+    # Finally, check the f1_score of the test data with our optimized model.
     test_f1_score = f1_score(y_test, random_search.best_estimator_.predict(X_test))
-    print(f"The f1_score of the test data is",round(test_f1_score, 3))
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    print(f"The f1_score of the test data is", round(test_f1_score, 3))
+
 
 # Make sure you call this script in the utils folder
-# Example: python3 data_eda.py ../data/processed/train_raw.csv ../data/eda_results/
-if __name__ == '__main__':
+# Example: python3 data_analysis.py ../data/processed/train_raw.csv ../data/processed/test_raw.csv ../data/results/
+if __name__ == "__main__":
     arguments = docopt(__doc__)
 
-    train_data = arguments['TRAINDATA'] # Download 1 dataset at a time
-    test_data = arguments['TESTDATA'] # Download 1 dataset at a time
-    out_path = arguments['OUTPUTFOLDER'][0]
-    ext_name = train_data_path.split('.')[-1]
+    train_data = arguments["TRAINDATA"]  # load 1 dataset at a time
+    test_data = arguments["TESTDATA"]  # load 1 dataset at a time
+    out_path = arguments["OUTPUTFOLDER"][0]
+    ext_name = train_data.split(".")[-1]
     perform_ml_analysis(train_data, test_data, out_path)

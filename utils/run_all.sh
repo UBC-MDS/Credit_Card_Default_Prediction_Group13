f1dc8682bb78f9d755dc35f736e12e30b1ef4ec3
Chester Wang <chesteraigo@dhcp-206-12-47-101.ubcsecure.wireless.ubc.ca>
2022-11-24 15:46:24 -0800

    minor update

--- utils/run_all.sh
+++ utils/run_all.sh
@@ -1,4 +1,4 @@
 python3 data_downloader.py https://archive.ics.uci.edu/ml/machine-learning-databases/00350/default%20of%20credit%20card%20clients.xls ../data/raw/
 python3 data_preprocessor.py ../data/raw/raw_data.xls ../data/processed/
 python3 data_eda.py ../data/processed/train_raw.csv ../data/eda_results/
-python3 data_analysis.py ../data/processed/train_cleaned.csv ../data/processed/test_cleaned.csv ../data/results/
+python3 data_analysis.py ../data/processed/train_cleaned.csv ../data/processed/test_cleaned.csv ../data/results

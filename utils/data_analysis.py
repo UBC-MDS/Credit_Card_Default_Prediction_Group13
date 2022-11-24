03b728afc1c30cc918cd651c66f5284f517fbfe5
qurat-azim <quratulainazim@gmail.com>
2022-11-23 19:19:22 -0800

    complete preliminary script

--- utils/data_analysis.py
+++ utils/data_analysis.py
@@ -1,11 +1,9 @@
-"""Usage: data_analysis.py [TRAIN_TRANS_X] [TEST_TRANS_X] [TRAIN_Y] [TEST_Y] [OUTPUTFOLDER] ...
+"""Usage: data_analysis.py [TRAINDATA] [TESTDATA] [OUTPUTFOLDER] ...
 
 Arguments:
-  TRAIN_TRANS_X        path of the transformed training data
-  TEST_TRANS_X         path of the transformed testing data
-  TRAINDATA            path of the training target
-  TESTDATA             path of the testing target
-  OUTPUTFOLDER         folder that stores the generated plots
+  TRAINDATA        path of the training data
+  TESTDATA         path of the testing data
+  OUTPUTFOLDER     folder that stores the generated plots
 
 
 """
@@ -17,7 +15,7 @@ from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler, OrdinalEncoder
 
 
-def perform_ml_analysis(train_data_path, test_data_path, out_folder):
+def perform_ml_analysis(X_train_enc, X_test_enc, Y_train_data_path, Y_test_data_path):
     if(not os.path.exists(out_folder)):
         os.mkdir(out_folder)
         
@@ -134,7 +132,182 @@ def perform_ml_analysis(train_data_path, test_data_path, out_folder):
     # Show the transformed data
     X_train_enc
     
+    import sys
+    from hashlib import sha1
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import pandas as pd
+    from sklearn.dummy import DummyClassifier
+    from sklearn.feature_extraction.text import CountVectorizer
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.model_selection import (
+        GridSearchCV,
+        RandomizedSearchCV,
+        cross_validate,
+        train_test_split,
+    )
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+    from sklearn.pipeline import Pipeline, make_pipeline
+    from sklearn.svm import SVC
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.linear_model import RidgeClassifier
+    from sklearn.ensemble import RandomForestClassifier
     
+    models = {
+        # "dummy": DummyClassifier(random_state=123),
+        "Decision Tree": DecisionTreeClassifier(random_state=123),
+        "KNN": KNeighborsClassifier(),
+        "RBF SVM": SVC(random_state=123),
+        "Logistic Regression": LogisticRegression(max_iter=1000, random_state=123),
+        "Ridge_cla": RidgeClassifier(random_state=123),
+        "RandomForest_cla": RandomForestClassifier(random_state=123),
+    }
+
+    from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+
+    classification_metrics = ["accuracy", "precision", "recall", "f1"]
+
+    from collections import defaultdict
+
+    cross_val_results = defaultdict(list)
+    for model in models:
+        cross_val_results[model].append(
+            cross_validate(
+                make_pipeline(preprocessor, models[model]),
+                X_train,
+                y_train,
+                cv=5,
+                return_train_score=True,
+                scoring=classification_metrics,
+            )
+        )
+        
+    # code below modified from :https://stackoverflow.com/questions/13575090/
+    #              construct-pandas-dataframe-from-items-in-nested-dictionary
+    cross_val_results_df = (
+        pd.concat(
+            {
+                key: pd.DataFrame(value[0]).agg(["mean", "std"])
+                for key, value in cross_val_results.items()
+            },
+            axis=0,
+        )
+        .T.style.format(
+            precision=2  # Pandas `.style` does not honor previous rounding via `.round()`
+        )
+        .background_gradient(
+            axis=None,
+            vmax=1,
+            vmin=0,  # Color cells based on the entire matrix rather than row/column-wise
+        )
+    )
+
+    cross_val_results_df
+    
+    # Selecting RandomForestClassifier for model hyperparameter optimization. 
+    from scipy.stats import expon, lognorm, loguniform, randint, uniform, norm
+    from sklearn.model_selection import RandomizedSearchCV
+
+    rf_cla = make_pipeline(preprocessor, RandomForestClassifier())
+    # rf_cla.fit(X_train, y_train)
+
+    param_dist = {"randomforestclassifier__max_depth": randint(1, 500),
+                 "randomforestclassifier__class_weight": ["balanced", None]}
+
+    random_search = RandomizedSearchCV(
+        rf_cla,
+        param_dist,
+        n_iter=20,
+        verbose=1,
+        n_jobs=-1,
+        random_state=123,
+        scoring="f1",
+    )
+
+    random_search.fit(X_train, y_train)
+
+    random_search.best_estimator_
+    
+    # This is an improvement compared to the mean validation f1 score using default parameters.
+    random_search.best_score_
+    
+    # This is the optimized hyperparameter.
+    random_search.best_params_
+    
+    # Generate a table showing feature importances.
+    feat_importance = random_search.best_estimator_.named_steps[
+        "randomforestclassifier"
+    ].feature_importances_
+
+    ohe_names = (
+        random_search.best_estimator_.named_steps["columntransformer"]
+        .named_transformers_["onehotencoder"]
+        .get_feature_names_out()
+        .tolist()
+    )
+
+    feat_names = numeric_features + ohe_names + passthrough_features
+
+    pd.DataFrame({"Feature": feat_names, "Importance": feat_importance}).sort_values(
+        by="Importance", ascending=False
+    )
+    
+    
+    # Add confusion matrix, predict score on test data.
+    from sklearn.metrics import ConfusionMatrixDisplay
+
+    cm = ConfusionMatrixDisplay.from_estimator(
+        random_search.best_estimator_, X_test, y_test, values_format="d", display_labels=["Non default", "default"]
+    )
+    
+    # Generate the classification report
+    from sklearn.metrics import classification_report
+    y_pred = random_search.best_estimator_.predict(X_test)
+    print(classification_report(y_test, y_pred, target_names=["Non default", "default"]))
+    
+    # Evaluate the Precision-Recall curve of the optimized model. 
+
+    from sklearn.metrics import precision_recall_curve
+
+    precision, recall, thresholds = precision_recall_curve(
+        y_test, random_search.best_estimator_.predict_proba(X_test)[:, 1]
+    )
+    plt.plot(precision, recall, label="logistic regression: PR curve")
+    plt.xlabel("Precision")
+    plt.ylabel("Recall")
+    plt.plot(
+        precision_score(y_test, random_search.best_estimator_.predict(X_test)),
+        recall_score(y_test, random_search.best_estimator_.predict(X_test)),
+        "or",
+        markersize=10,
+        label="threshold 0.5",
+    )
+    plt.legend(loc="best");
+    
+    # Evaluate the Receiver Operating Characteristic (ROC) curve of the optimized model. 
+    from sklearn.metrics import roc_curve
+
+    fpr, tpr, thresholds = roc_curve(y_test, random_search.best_estimator_.predict_proba(X_test)[:, 1])
+    plt.plot(fpr, tpr, label="ROC Curve")
+    plt.xlabel("FPR")
+    plt.ylabel("TPR (recall)")
+
+    default_threshold = np.argmin(np.abs(thresholds - 0.5))
+
+    plt.plot(
+        fpr[default_threshold],
+        tpr[default_threshold],
+        "or",
+        markersize=10,
+        label="threshold 0.5",
+    )
+    plt.legend(loc="best");
+    
+    # Finally, check the f1_score of the test data with our optimized model. 
+    test_f1_score = f1_score(y_test, random_search.best_estimator_.predict(X_test))
+    print(f"The f1_score of the test data is",round(test_f1_score, 3))
     
     
     
@@ -151,10 +324,8 @@ def perform_ml_analysis(train_data_path, test_data_path, out_folder):
 if __name__ == '__main__':
     arguments = docopt(__doc__)
 
-    X_train_data_path = arguments['TRAIN_TRANS_X'] # Download 1 dataset at a time
-    X_test_data_path = arguments['TEST_TRANS_X'] # Download 1 dataset at a time
-    Y_train_data_path = arguments['TRAIN_Y'] # Download 1 dataset at a time
-    Y_test_data_path = arguments['TEST_Y'] # Download 1 dataset at a time
+    train_data = arguments['TRAINDATA'] # Download 1 dataset at a time
+    test_data = arguments['TESTDATA'] # Download 1 dataset at a time
     out_path = arguments['OUTPUTFOLDER'][0]
     ext_name = train_data_path.split('.')[-1]
-    perform_ml_analysis(X_train_data_path, X_test_data_path, Y_train_data_path, Y_test_data_path)
+    perform_ml_analysis(train_data, test_data, out_path)
